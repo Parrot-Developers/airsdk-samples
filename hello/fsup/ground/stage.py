457bab32298bc5b1cc106b27dc4e45641514779b
Ronan Chauvin <ronan.chauvin@parrot.com>
2020-07-28 14:47:26 +0200

    [DEV] samples: hello: fsup: handle count of say

--- hello/fsup/ground/stage.py
+++ hello/fsup/ground/stage.py
@@ -2,6 +2,7 @@
 from fsup.genstate import (State, guidance_modes)
 import colibrylite.estimation_mode_pb2 as cbry_est
 import hello.guidance.hello_ground_mode_pb2 as HelloGroundMode
+import hello.messages.com_parrot_mission_samples_hello_pb2 as HelloMessages
 
 from fsup.missions.default.ground.stage import GROUND_STAGE as DEF_GROUND_STAGE
 
@@ -26,6 +27,11 @@ class Idle(State):
 @guidance_modes(UID + '_ground')
 class Say(State):
     def enter(self, msg):
+        self.gdnc_grd_svc = self.mc.attach_client_service_pair(
+            self.mc.gdnc_channel,
+            HelloGroundMode,
+            forward_events=True)
+
         m = self.mc.dctl.cmd.alloc()
         m.set_estimation_mode.mode = cbry_est.MOTORS_STOPPED
         self.mc.send(m)
@@ -34,6 +40,16 @@ class Say(State):
         m.set_mode.mode = UID + '_ground'
         m.set_mode.config.Pack(HelloGroundMode.Config(say=True))
         self.mc.send(m)
+
+    def exit(self, msg):
+        self.mc.detach_client_service_pair(self.gdnc_grd_svc)
+        self.gdnc_grd_svc = None
+
+    def step(self, msg):
+        msgname = msg.WhichOneof('id')
+        if msgname == "count" and isinstance(msg, HelloGroundMode.Event):
+            #TODO: send HelloMessages event count.
+            self.log.info('%s', msg)
 
 IDLE_STATE = {
     'name': 'idle',
