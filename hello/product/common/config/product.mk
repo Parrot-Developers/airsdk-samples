9261f9b248445d6c5fc813fe7bbd9cad2b83aea0
Ronan Chauvin <ronan.chauvin@parrot.com>
2020-12-21 16:28:45 +0100

    [DEV] samples: hello: include buildext mission config
    This will:
    * Setup ldflags
    * Setup pyhton3 symlink

--- hello/product/common/config/product.mk
+++ hello/product/common/config/product.mk
@@ -1,6 +1,7 @@
 COMMON_CONFIG_DIR := $(call my-dir)
 
-TARGET_GLOBAL_LDFLAGS := -Wl,-rpath=\$$ORIGIN:\$$ORIGIN/../lib
-
 # Add Common skeleton
 TARGET_SKEL_DIRS += $(COMMON_CONFIG_DIR)/../skel
+
+# Include buildext mission config modules
+include $(ALCHEMY_WORKSPACE_DIR)/build/dragon_buildext_mission/product.mk
