ceec43a59d087b667de83d09404c8d44205b7764
Georges Boumis <georges.boumis@parrot.com>
2023-03-07 18:08:02 +0100

    [CONFIG] classic/pc: add variants to TARGET_SKEL_DIRS
    Change-Id: I75b70b967078979c30dd10462ec6a033fdcbf36c

--- hello/product/pc/config/product.mk
+++ hello/product/pc/config/product.mk
@@ -1,7 +1,9 @@
-
 HELLO_PC_CONFIG_DIR := $(call my-dir)
 
 TARGET_SDK_DIRS = $(ALCHEMY_WORKSPACE_DIR)/sdk/pc
+
+# Add pc skeleton
+TARGET_SKEL_DIRS += $(HELLO_PC_CONFIG_DIR)/../skel
 
 # Include common config modules
 include $(HELLO_PC_CONFIG_DIR)/../../common/config/product.mk
